6c9d0614019c99c7c67b8c961b9e97b7d6d0ceed
Deytrino <43432770+Deytrino@users.noreply.github.com>
2021-10-04 12:37:00 +0000

    Added a button and an input field.

--- main.py
+++ main.py
@@ -7,10 +7,28 @@ class MyApp:
     root.geometry("500x400")
     root.maxsize(600,400)
 
-    label = Label(root, text="Some label text")
+
+    self.label_text = StringVar()
+    label = Label(root, text="Some label text", textvariable=self.label_text)
     label.pack()
 
-    label["text"] = "New label text"
+    #label["text"] = "New label text"
+    #label["font"] = ("Courier", 40)
+    label.configure(text = "New label text", font = ("Courier", 40))
+
+    self.entry_text = StringVar()
+    entry = Entry(root, textvariable=self.entry_text)
+    entry.pack()
+
+    button = Button(root, text="Press me", command=self.press_button)
+    button.pack()
+
+  def press_button(self):
+    text = self.entry_text.get()
+    self.label_text.set(text)
+    print("Button pressed")
+
+
 
 root = Tk()
 MyApp(root)
